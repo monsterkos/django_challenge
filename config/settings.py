c3a603488c3df6104338d48636abee91fcb7788d
monsterkos <monsterkos10@gmail.com>
2020-09-25 22:22:17 +0900

    update for deploy2

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import my_settings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -100,7 +99,16 @@ if DEBUG:
         }
     }
 else:
-    DATABASES = my_settings.DATABASES
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            "HOST":"airbnb-clone.ccbh8epimdms.ap-northeast-2.rds.amazonaws.com",
+            'NAME': "airbnb-clone",
+            "USER":"postgresql",
+            'PASSWORD':"rnjsdhtjd2",
+            "PORT":"5432",
+        }
+    }
 
 
 
